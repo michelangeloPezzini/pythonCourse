5837a8ef9676f1e6212b23ff1684606015cb33a9
Michelangelo Giuseppe Pezzini <lds.pezzini@gmail.com>
2022-05-23 21:58:55 -0300

    week6

--- Curso em video/aula-13/cursoPython.py
+++ Curso em video/aula-13/cursoPython.py
@@ -109,3 +109,18 @@ while c > 0:
     c = c - 1    
 print(f)
 print("Fim") """
+
+
+sequencia = int(input("Digite o valor da sequencia fibonati: "))
+t1 = 0
+t2 = 1
+print("{} → {}".format(t1,t2),end="")
+
+cont = 3
+while cont <= sequencia:
+    t3 = t1 + t2
+    print(" → {}".format(t3),end="")
+    t1 = t2
+    t2 = t3
+    cont = cont + 1
+print("\nFim")
