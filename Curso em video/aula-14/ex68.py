3fd84978c442cae714d3dbde27c4b9d579ee4147
michelangelo.pezzini@compasso.com.br <michelangelo.pezzini@compasso.com.br>
2022-06-01 11:01:30 -0300

    Update ex68.py

--- Curso em video/aula-14/ex68.py
+++ Curso em video/aula-14/ex68.py
@@ -1,10 +1,5 @@
 import random
-parOuImpar = str(input("Par ou impar? ")).upper()
-numero = int(input("Digite seu numero: "))
-var = random.randint(1,11)
-resultado = var + numero
-print(f"{var} e {numero} = {numero + var}")
-if parOuImpar == "PAR" and  resultado % 2 == 0:
-  print("Você Ganhou PAR")
-else:
-  print("Você perdeu!")
+#Implementar melhor o jogo do par ou impar
+while True:
+  jogador = str(input("Digite um numero: "))
+  computador = random.randint(0,11)
