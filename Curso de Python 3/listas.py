107cfb9d1574fd025eed9ad78c1983c8b2d0c8e1
Michelangelo Giuseppe Pezzini <lds.pezzini@gmail.com>
2022-06-14 23:50:35 -0300

    week nine

---
+++ Curso de Python 3/listas.py
@@ -0,0 +1,50 @@
+""" lista = ["a", "b", "c", "d", 123]
+print(lista[::2])
+print(lista[-1])
+
+listaDois = [1,2,3,4,5]
+listaDois.append(6)
+listaDois.insert(2, "Teste")
+listaDois.pop()
+
+print(listaDois)
+
+for item in listaDois:
+  print(item) """
+
+import random
+palavras = ["CORRER", "GATO", "IGREJA", "NADADOR", "ESCRITURAS"] 
+palavra = random.choice(palavras)
+digitadas = []
+vida = 5
+while True:
+  user = str(input("Digite somente uma letra: ")).upper()
+  if len(user) > 1:
+    print("Digite apenas uma letra, tente novamente.")
+    continue
+  
+  digitadas.append(user)
+  if user in palavra:
+    print(f"A letra {user} existe na palavra secreta.")
+  else: 
+    print(f"A letra {user} não existe na palavra secreta.")
+    digitadas.pop()
+    vida = vida - 1
+    print(f"Você ainda tem {vida} chances!!!")
+  
+  letraSecreta = ""
+  for letra in palavra:
+    if letra in digitadas:
+      letraSecreta = letraSecreta + letra
+    else: 
+      letraSecreta = letraSecreta + "_"
+
+  if letraSecreta == palavra:
+    print(letraSecreta)
+    print("Você acertou, parabens!!!")
+    break
+  elif vida == 0:
+    print("Acabou suas tentativas")
+    break
+  else:
+    print(letraSecreta)
---
+++ web and computer programming/weekNine/ex01.py
@@ -0,0 +1,8 @@
+mixDeFrutas = []
+while True:
+  frutas =  str(input("Me diga 3 frutas? "))
+  mixDeFrutas.append(frutas)
+  if len(mixDeFrutas) >= 3:
+    break
+  else:
+    print(mixDeFrutas) 
